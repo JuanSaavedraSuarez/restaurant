7788b6bfb4070d46e46a128adc85a67c2109cea9
j s <jlssz@proton.me>
2025-07-28 20:31:24 -0700

    initial commit

---
+++ Restaurant.hpp
@@ -0,0 +1,18 @@
+#ifndef RESTAURANT_H
+#define RESTAURANT_H
+
+#include <vector>
+#include <memory>
+#include "Table.hpp"
+#include "Customer.hpp"
+
+
+class Restaurant {
+private:
+    std::vector<std::unique_ptr<Table>> tables;
+
+public:
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Restaurant.hpp"
+
+using namespace std;
+int main() {
+  // Initialize Restaurant with 2 tables
+  
+  // Create customers and make reservations
+  
+  // Release a table and demonstrate waitlist notification
+
+  return 0;
+}
