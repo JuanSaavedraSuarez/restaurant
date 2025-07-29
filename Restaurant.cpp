99a57e3e691e7aa9238d78a473609e10d97befe2
j s <jlssz@proton.me>
2025-07-29 12:41:43 -0700

    Added reservation logic

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -3,5 +3,25 @@
 #include "removeElement.hpp"
 
 #include <iostream>
+#include <memory>
 
 using namespace std;
+
+Restaurant::Restaurant(int initialTableCount) {
+    for (int i = 0; i < initialTableCount; i++) {
+        tables.push_back(std::make_unique<Table>(i));
+    }
+}
+
+bool Restaurant::reserveTable(std::shared_ptr<Customer>& customer) {
+    for (std::unique_ptr<Table>& table : tables) {
+        if ((*table).getIsAvailable()) {
+            (*table).reserve();
+            activeCustomers.push_back((customer));
+            return true;
+        }
+    }
+    waitlist.push_back(customer);
+    return false;
+}
+
--- Restaurant.hpp
+++ Restaurant.hpp
@@ -10,10 +10,12 @@
 class Restaurant {
 private:
     std::vector<std::unique_ptr<Table>> tables;
+    std::vector<std::shared_ptr<Customer>> activeCustomers;
     std::vector<std::weak_ptr<Customer>> waitlist;
 
 public:
-
+    Restaurant(int initialTableCount);
+    bool reserveTable(std::shared_ptr<Customer>& customer);
 };
 
 #endif
